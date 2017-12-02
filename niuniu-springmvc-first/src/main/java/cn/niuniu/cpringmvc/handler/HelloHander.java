1249202c15869b8964b59c326519d3233c0c52b6
huanglijun <hlj7239011>
2017-12-02 21:57:33 +0800

    fix bug

--- niuniu-springmvc-first/src/main/java/cn/niuniu/cpringmvc/handler/HelloHander.java
+++ niuniu-springmvc-first/src/main/java/cn/niuniu/cpringmvc/handler/HelloHander.java
@@ -21,7 +21,7 @@ public class HelloHander implements Controller {
      */
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("index");   //添加 视图的名称
+        mv.setViewName("hello");   //添加 视图的名称
         mv.addObject("msg","hi 妞妞!");  //添加 模型数据  (实际开发 都调后面逻辑返回的)
         return mv;
     }
